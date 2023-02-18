3c502d797bb06ed503d15685f6e43b641a356a9b
Mario Alves <marchetti2@gmail.com>
2023-02-18 01:38:29 -0300

    lint

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -13,15 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    
     @IBAction func predictPressed(_ sender: Any) {
-    
-    
+        
+        
     }
     
 }
